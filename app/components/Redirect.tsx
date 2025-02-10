c0212bdccb075eec53d3ce46a2cb1c584c732a57
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-02-10 23:26:22 +0530

    complete

--- app/components/Redirect.tsx
+++ app/components/Redirect.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react"
 
 // this is redirect component
+// this component is used to redirect user to dashboard if user is logged in
 export default function Redirect(){
   const session = useSession();
   const router = useRouter();
